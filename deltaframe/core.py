1f1a8056e62d85e8d84a990abccbf3d910eae52b
tkanngiesser <tinokanngiesser@gmail.com>
2020-12-13 07:40:01 +0100

    update docstrings

--- deltaframe/core.py
+++ deltaframe/core.py
@@ -8,7 +8,7 @@ import numpy as np
 
 # Cell
 def get_added(df_old, df_new, unique_id, trans_col="transaction", trans_vaL="added"):
-    """get rows in df_new which are not in df_old
+    """Returns the added rows in df_new
 
     Parameters
     ----------
@@ -19,15 +19,16 @@ def get_added(df_old, df_new, unique_id, trans_col="transaction", trans_vaL="add
         unique_id: str or list
             unique identifier(s)
         trans_col: str
-            name of column to track transaction
+            name of column to track transaction (default is "transaction")
         trans_val: str
-            name of value to reflect transaction status "added"
+            name of value to reflect transaction status (default is "added")
 
     Returns
     -------
     pd.DataFrame
         dataframe that contains added rows
     """
+
     cols = list(df_old.columns)
     new_rows = (pd
                 .merge(df_old, df_new, how="outer", on=unique_id, indicator=True, suffixes=("_foo",""))
@@ -39,7 +40,26 @@ def get_added(df_old, df_new, unique_id, trans_col="transaction", trans_vaL="add
 
 # Cell
 def get_removed(df_old, df_new, unique_id, trans_col="transaction", trans_val="removed"):
-    """Returns the removed rows that are not any longer in df_new"""
+    """Returns the removed rows that are not any longer in df_new
+
+    Parameters
+    ----------
+        df_old : pd.DataFrame
+            dataframe with previous information
+        df_new: pd.DataFrame
+            dataframe with new information
+        unique_id: str or list
+            unique identifier(s)
+        trans_col: str
+            name of column to track transaction (default is "transaction")
+        trans_val: str
+            name of value to reflect transaction status (default is "removed")
+    Returns
+    -------
+    pd.DataFrame
+        dataframe that contains added rows
+    """
+
     cols = list(df_old.columns)
     removed_rows = (pd
                 .merge(df_new, df_old, how="outer", on=unique_id, indicator=True, suffixes=("_foo",""))
@@ -51,7 +71,28 @@ def get_removed(df_old, df_new, unique_id, trans_col="transaction", trans_val="r
 
 # Cell
 def get_modified(df_old, df_new, unique_id, added_rows=None, trans_col="transaction", trans_val="modified"):
-    """Returns the modified rows in df_new"""
+    """Returns the modified rows in df_new
+
+    Parameters
+    ----------
+        df_old : pd.DataFrame
+            dataframe with previous information
+        df_new: pd.DataFrame
+            dataframe with new information
+        unique_id: str or list
+            unique identifier(s)
+        added_rows: pd.DataFrame
+            added rows from calling get_added function
+        trans_col: str
+            name of column to track transaction (default is "transaction")
+        trans_val: str
+            name of value to reflect transaction status (default is "modified")
+    Returns
+    -------
+    pd.DataFrame
+        dataframe that contains added rows
+    """
+
     cols = list(df_new.columns)
     if added_rows is not None:
         df_new = df_new[~df_new.isin(list(added_rows[unique_id].values))].dropna()
@@ -63,6 +104,32 @@ def get_modified(df_old, df_new, unique_id, added_rows=None, trans_col="transact
 
 # Cell
 def get_delta(df_old, df_new, unique_id, sort_by=None, trans_col="transaction", trans_val_added="added", trans_val_removed="removed", trans_val_modified="modified"):
+    """Get delta (added, removed, modified) betwen df_old and df_new
+
+    Parameters
+    ----------
+        df_old : pd.DataFrame
+            dataframe with previous information
+        df_new: pd.DataFrame
+            dataframe with new information
+        unique_id: str or list
+            unique identifier(s)
+        added_rows: pd.DataFrame
+            added rows from calling get_added function
+        trans_col: str
+            name of column to track transaction (default is "transaction")
+        trans_val_added: str
+            name of value to reflect transaction status (default is "added")
+        trans_val_removed: str
+            name of value to reflect transaction status (default is "removed")
+        trans_val_modified: str
+            name of value to reflect transaction status (default is "modified")
+    Returns
+    -------
+    pd.DataFrame
+        dataframe that contains added delta between df_old and df_new
+    """
+
     added_rows = get_added(df_old=df_old, df_new=df_new, unique_id="id",trans_col=trans_col, trans_vaL=trans_val_added)
     removed_rows = get_removed(df_old=df_old, df_new=df_new, unique_id="id", trans_col=trans_col, trans_val=trans_val_removed)
     modified_rows = get_modified(df_old=df_old, df_new=df_new, unique_id="id", added_rows=added_rows, trans_col=trans_col, trans_val=trans_val_modified)
